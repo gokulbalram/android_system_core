cbcfa617da9052c8a10c8da61a784ba49d0824f1
Park Ju Hyung <qkrwngud825@gmail.com>
2017-11-06 20:30:39 +0900

    init: workaround SafetyNet check
    Doing this in the userspace allows more properties to be spoofed
    and eliminate the needs for a hack in the kernel.
    
    Change-Id: I25230b8e14dbc7ced02451dd8dfb03848452461c
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -788,6 +788,39 @@ static void load_override_properties() {
     }
 }
 
+/* From Magisk@jni/magiskhide/hide_utils.c */
+static const char *snet_prop_key[] = {
+	"ro.boot.vbmeta.device_state",
+	"ro.boot.verifiedbootstate",
+	"ro.boot.flash.locked",
+	"ro.boot.selinux",
+	"ro.boot.veritymode",
+	"ro.boot.warranty_bit",
+	"ro.warranty_bit",
+	"ro.debuggable",
+	"ro.secure",
+	"ro.build.type",
+	"ro.build.tags",
+	"ro.build.selinux",
+	NULL
+};
+
+static const char *snet_prop_value[] = {
+	"locked",
+	"green",
+	"1",
+	"enforcing",
+	"enforcing",
+	"0",
+	"0",
+	"0",
+	"1",
+	"user",
+	"release-keys",
+	"1",
+	NULL
+};
+
 // If the ro.product.[brand|device|manufacturer|model|name] properties have not been explicitly
 // set, derive them from ro.product.${partition}.* properties
 static void property_initialize_ro_product_props() {
@@ -936,6 +969,11 @@ void property_load_boot_defaults(bool load_debug_prop) {
 
     property_initialize_ro_product_props();
     property_derive_build_fingerprint();
+
+    // Workaround SafetyNet
+    workaround_snet_properties();
+
+}
 
     if (android::base::GetBoolProperty("ro.persistent_properties.ready", false)) {
         update_sys_usb_config();
